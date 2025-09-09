78dc9898fd86e6331dea70ac489ec4121c9cf85f
raghav <134798855+raghavnimbalkar1@users.noreply.github.com>
2025-09-10 01:16:09 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <SFML/Graphics.hpp>
 
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "My C++ Game Engine");
+    window.setFramerateLimit(60);
+  
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear(sf::Color(20, 20, 40));
+        window.display();
+    }
+    return 0;
+}
