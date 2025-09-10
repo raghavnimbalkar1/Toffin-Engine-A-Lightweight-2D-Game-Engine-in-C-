c03ec813eb0a50b538b6df8f63337ff0c454be60
raghavnimbalkar1 <raghav.nimbalkar@gmail.com>
2025-09-10 16:27:26 +0530

    Cleaning up

--- build/CMakeFiles/ToffinEngine.dir/home/skinny/Documents/Code/Toffin-Engine-A-Lightweight-2D-Game-Engine-in-C-/main.cpp.o.d
+++ build/CMakeFiles/ToffinEngine.dir/home/skinny/Documents/Code/Toffin-Engine-A-Lightweight-2D-Game-Engine-in-C-/main.cpp.o.d
@@ -234,4 +234,29 @@ CMakeFiles/ToffinEngine.dir/home/skinny/Documents/Code/Toffin-Engine-A-Lightweig
  /usr/include/SFML/Graphics/Glsl.hpp /usr/include/SFML/Graphics/Glsl.inl \
  /usr/include/SFML/Graphics/Sprite.hpp \
  /usr/include/SFML/Graphics/Text.hpp \
- /usr/include/SFML/Graphics/VertexBuffer.hpp
+ /usr/include/SFML/Graphics/VertexBuffer.hpp \
+ /home/skinny/Documents/Code/Toffin-Engine-A-Lightweight-2D-Game-Engine-in-C-/objects/GameObject.h \
+ /home/skinny/Documents/Code/Toffin-Engine-A-Lightweight-2D-Game-Engine-in-C-/objects/../math/Vector2D.h \
+ /usr/include/c++/13/cmath /usr/include/math.h \
+ /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-narrow.h \
+ /usr/include/x86_64-linux-gnu/bits/iscanonical.h \
+ /usr/include/c++/13/bits/specfun.h /usr/include/c++/13/limits \
+ /usr/include/c++/13/tr1/gamma.tcc \
+ /usr/include/c++/13/tr1/special_function_util.h \
+ /usr/include/c++/13/tr1/bessel_function.tcc \
+ /usr/include/c++/13/tr1/beta_function.tcc \
+ /usr/include/c++/13/tr1/ell_integral.tcc \
+ /usr/include/c++/13/tr1/exp_integral.tcc \
+ /usr/include/c++/13/tr1/hypergeometric.tcc \
+ /usr/include/c++/13/tr1/legendre_function.tcc \
+ /usr/include/c++/13/tr1/modified_bessel_func.tcc \
+ /usr/include/c++/13/tr1/poly_hermite.tcc \
+ /usr/include/c++/13/tr1/poly_laguerre.tcc \
+ /usr/include/c++/13/tr1/riemann_zeta.tcc
